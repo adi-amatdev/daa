9f998aad7a5af1329d5d9f1ad05fa72abda4e517
adi-amatdev <1rn20cs169.udupiaadityaacharya@gmail.com>
2022-07-10 22:49:37 +0530

    implementation
    greedy knapsack algo in c

---
+++ lab/greedy knapsack.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct obj{
+    int  flag;// w corresponds to c
+    float ratio,x,p,w;
+
+}sack;
+void calcks(sack objects[],int   n,float c);
+sack* find_maxrat(sack objects[],float  n);
+int main (){
+    float c;
+    int n;
+    printf("Enter number of objects : \n");
+    scanf("%d",&n);
+    sack objects[n];
+    printf("Enter the weights and profit of the %d objects \n",n);
+    for (int i=0; i<n; i++){
+        printf("for object number %d : \n",i+1);
+        scanf("%f%f",&objects[i].w,&objects[i].p);
+        objects[i].flag =0;
+        objects[i].x =0;
+    }
+    for(int i=0; i<n; i++){
+        objects[i].ratio = (objects[i].p)/(objects[i].w); //error  may appear here 
+    }
+
+    printf("Enter the capacity of the knapsack : \n");
+    scanf ("%f",&c);
+
+    calcks(objects,n,c);
+
+    return 0;
+}
+void calcks(sack objects[],int  n,float c){
+    float  sum =0;
+    float u=0;
+  
+    while(u<= c){
+        sack *item = find_maxrat(objects,n);
+
+        if(item->w <= c-u){
+                item->x =1;                  //x is not updating 
+                u += item->w;
+        }
+        else {
+            item->x = ((c-u)/item->w); 
+            printf("%f",item->x)   ;    //x is not updating
+            break;
+        }
+    }
+    printf("\nThe objects in the knapsack are : \n");
+    for(int i=0; i<n; i++){
+        if(objects[i].x > 0){
+            printf("%d \t",i+1);
+        }
+    }
+    printf("\n");
+    printf("The max profit is : ");
+    for(int i=0; i<n; i++){
+        sum += ((objects[i].p)*(objects[i].x));
+    }
+    printf("%f",sum);
+    printf("\n");
+}
+sack* find_maxrat(sack objects[],float n){
+    
+    sack *large = &objects[0];
+    for(int i=1;i<n;i++){
+        if(objects[i].ratio > large->ratio && objects[i].flag != 1){
+            large = &objects[i];
+        }
+    }
+    large->flag =1 ;
+    return large ;
+}
